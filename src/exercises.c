e1340b8416ee8565b05d18ed167c33efe4e5aca5
Samuel_Jimenez <samuel.jimenezs.col@upb.edu.co>
2025-04-22 17:18:37 -0500

    se agrega la segunda actividad

--- src/exercises.c
+++ src/exercises.c
@@ -8,7 +8,8 @@ int calcSum(int arr[], int n)
 {
     int sum = 0; // Inicializamos la suma en 0
     
-    for (int i = 0; i < n; i++) { // Iteramos por el arreglo
+    for (int i = 0; i < n; i++) // Iteramos por el arreglo
+    { 
         sum += arr[i]; // Sumamos cada elemento al total
     }
     
@@ -17,7 +18,16 @@ int calcSum(int arr[], int n)
 
 int findMax(int arr[], int n)
 {
-    // Escriba aquí su código
+    int max = arr[0]; // Inicializamos max con el primer elemento del arreglo
+    
+    for (int i = 1; i < n; i++) { // Iteramos desde el segundo elemento
+        if (arr[i] > max) { // Si encontramos un valor mayor
+            max = arr[i]; // Actualizamos max
+        }
+    }
+    
+    return max; // Retornamos el valor máximo encontrado
+
 }
 
 float calcAverage(int arr[], int n)
